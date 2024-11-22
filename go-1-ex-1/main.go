94dd4c3757b3cea03bc77ab64a23273fecdb5d73
Ioannis Ligkas <Ioannis_Ligkas@sluz.ch>
2024-11-22 15:18:55 +0100

    Finished ex-1-1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var firstName, lastName, dayOfBirth, monthOfBirth, yearOfBirth, numberOfSiblings, heightInMeters, zodiacSign = "Ioannis", "Ligkas", 2, 12, 2007, 1, 1.89, '\u2650'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
