3cc3644a2437212610cee10835465ae803277ed3
Ioannis Ligkas <Ioannis_Ligkas@sluz.ch>
2024-11-22 17:04:42 +0100

    Finished ex-1-3

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -11,11 +12,13 @@ func main() {
 	var when = time.Now()
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	n, error := fmt.Fprintln(os.Stdout, "the dice shows", eyes, "eyes")
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	n2, error2 := fmt.Fprintln(os.Stderr, "the dice was rolled at", when)
 
 	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
+	// go run ex3/main.go>eyes.txt es steht in die standard ausgabe weil für mich der Augenzahl frei zum sehen sein soll
+	// go run ex3/main.go2>dice.log es steht in die standard error weil ein log öffters für admins ist
+	fmt.Print(n, n2, error, error2)
 }
